ea09956dca373ec1746b551917c530125458149d
jghagemann <hagejotage@hotmail.com>
2021-06-03 15:52:56 -0300

    Criado layout Cadastro de Locais

---
+++ Eventos/app/src/main/java/br/senai/sc/eventos/CadastroLocalActivity.java
@@ -0,0 +1,14 @@
+package br.senai.sc.eventos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroLocalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_local);
+    }
+}
---
+++ Eventos/app/src/main/java/br/senai/sc/eventos/ListarLocaisActivity.java
@@ -0,0 +1,14 @@
+package br.senai.sc.eventos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListarLocaisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_locais);
+    }
+}
